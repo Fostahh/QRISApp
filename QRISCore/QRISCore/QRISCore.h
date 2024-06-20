e2b6631dccfe26150ba5a4f40b1d417c1fda777f
Azmi Muhammad <m.azmi65@yahoo.com>
2024-06-20 11:44:54 +0700

    [ADD] QRISCore Framework

---
+++ QRISCore/QRISCore/QRISCore.h
@@ -0,0 +1,18 @@
+//
+//  QRISCore.h
+//  QRISCore
+//
+//  Created by Azmi Muhammad on 20/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for QRISCore.
+FOUNDATION_EXPORT double QRISCoreVersionNumber;
+
+//! Project version string for QRISCore.
+FOUNDATION_EXPORT const unsigned char QRISCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRISCore/PublicHeader.h>
+
+
